d6548269de58465f24f3daa0b289a255afbe9aca
Mythologia <brisingr0306@hotmail.com>
2016-10-08 22:12:22 -0500

    Implement twilio twiml msg

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var BodyParser = require('body-parser');
 var Swaggerize = require('swaggerize-express');
 var swaggerUi = require('swaggerize-ui');
 var Path = require('path');
+var twilio = require('twilio')
 
 var App = Express();
 
@@ -25,6 +26,20 @@ App.use(Swaggerize({
 App.use('/docs', swaggerUi({
     docs: '/swagger'
 }));
+
+App.post('/route', function(req, res) {
+    var twiml = new twilio.TwimlResponse();
+    if (req.body.Body == 'hello') {
+        twiml.message('Hi!');
+    } else if(req.body.Body == 'bye') {
+        twiml.message('Goodbye');
+    } else {
+        twiml.message('No Body param match, Twilio sends this in the request to your server.');
+    }
+    res.writeHead(200, {'Content-Type': 'text/xml'});
+    res.end(twiml.toString());
+});
+
 
 Server.listen(port, function () {
 });
